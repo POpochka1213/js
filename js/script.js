6f1a77d73bcd2baab4c7d5bd8f9726ef3e197c02
POpochka1213 <timerhanqwert@gmail.com>
2025-04-03 02:08:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -61,3 +61,83 @@ function MATH() {
     let pr = (parseInt(a) * parseInt(b));
     alert(`сумма равна ${sum} разность равна ${raz} частное равно ${chast} произведение равно ${pr}`);
 }
+
+// Задание 1 Практика 3
+
+function money() {
+    const itemPrice = parseFloat(prompt("Введите стоимость товара:"));
+    const clientMoney = parseFloat(prompt("Введите количество денег клиента:"));
+
+    if (itemPrice === clientMoney) {
+        alert("Покупка совершена");
+    } else if (itemPrice > clientMoney) {
+        const missingAmount = itemPrice - clientMoney;
+        alert("Для покупки не хватает " + missingAmount + " р.");
+    } else {
+        const changeAmount = clientMoney - itemPrice;
+        alert("Покупка совершена. Сдача " + changeAmount + " р.");
+    }
+}
+
+// Задание 2 практика 3
+
+function moreornot() {
+    const number = parseFloat(prompt("Введите число:"));
+
+    if (number > 0) {
+        alert(1);
+    } else if (number < 0) {
+        alert(-1);
+    } else {
+        alert(0);
+    }
+}
+
+// Задание 3 Практика 3
+
+function Sum() {
+    let a = prompt('a');
+    let b = prompt('b');
+    let result = (Number(a) + Number(b) < 4) ? 'Мало' : 'Много';
+    alert(result);
+}
+
+// Задание 4 практика 3
+
+function levelUser() {
+    let login = prompt('login');
+
+    let message =
+        login == 'Сотрудник' ? 'Привет' :
+            login == 'Директор' ? 'Здравствуйте' :
+                login == '' ? 'Нет логина' :
+                    'ЛЭЭЭЭЭ братишка кто ты';
+
+    alert(message);
+}
+
+
+
+// 5. Задание 5 практика 3
+
+function Login() {
+    let login = prompt("Введите логин:");
+
+    if (login === "Админ") {
+        let password = prompt("Введите пароль:");
+
+        if (password === "Я главный") {
+            alert("Здравствуйте!");
+        } else if (password === null || password === "") {
+            alert("Отменено");
+        } else {
+            alert("Неверный пароль");
+        }
+    } else if (login === null || login === "") {
+        alert("Отменено");
+    } else {
+        alert("Я вас не знаю");
+    }
+}
+
+
